6685f76ea3ebf9d04ea0d399137d25daa7248355
Benyo27 <bgonzalezb@uc.cl>
2024-06-24 17:34:09 -0400

    feat(filter series): by stars

--- components/SeriesList/SeriesList.tsx
+++ components/SeriesList/SeriesList.tsx
@@ -11,6 +11,7 @@ export default function SeriesList() {
   const [filterName, setFilterName] = useState('');
   const [filterStreamingService, setFilterStreamingService] = useState('');
   const [filterCategory, setFilterCategory] = useState('');
+  const [filterStars, setFilterStars] = useState(0);
   const [filteredSeries, setFilteredSeries] = useState([]);
   useEffect(() => {
     const fetchSeries = async () => {
@@ -31,11 +32,13 @@ export default function SeriesList() {
       return (
         serie.name.toLowerCase().includes(filterName.toLowerCase()) &&
         serie.streaming_service.toLowerCase().includes(filterStreamingService.toLowerCase()) &&
-        serie.category.toLowerCase().includes(filterCategory.toLowerCase())
+        serie.category.toLowerCase().includes(filterCategory.toLowerCase()) &&
+        serie.stars >= filterStars
       );
     });
     setFilteredSeries(filtered);
-  }, [filterName, filterStreamingService, filterCategory, series]);
+    console.log(filterStars)
+  }, [filterName, filterStreamingService, filterCategory, filterStars, series]);
 
   return (
     <div className="flex flex-col gap-12">
@@ -48,7 +51,7 @@ export default function SeriesList() {
         />
         <div className="flex gap-10">
           <select className="rounded-md px-2 py-2 bg-inherit border w-full" onChange={(e) => setFilterStreamingService(e.target.value)} value={filterStreamingService}> 
-            <option value="">Todos los servicios</option>
+            <option value="">Servicio de Streaming</option>
             <option value="Netflix">Netflix</option>
             <option value="Amazon Prime Video">Amazon Prime Video</option>
             <option value="Disney+">Disney+</option>
@@ -56,7 +59,7 @@ export default function SeriesList() {
             <option value="Apple TV+">Apple TV+</option>
           </select>
           <select className="rounded-md px-2 py-2 bg-inherit border w-full" onChange={(e) => setFilterCategory(e.target.value)} value={filterCategory}> 
-            <option value="">Todos las categorías</option>
+            <option value="">Categoría</option>
             <option value="Ciencia Ficción">Ciencia Ficción</option>
             <option value="Terror">Terror</option>
             <option value="Misterio">Misterio</option>
@@ -65,6 +68,18 @@ export default function SeriesList() {
             <option value="Comedia">Comedia</option>
             <option value="Fantasía">Fantasía</option>
             <option value="Romántica">Romántica</option>
+          </select>
+          <select className="rounded-md px-2 py-2 bg-inherit border w-full" onChange={(e) => setFilterStars(parseInt(e.target.value))} value={filterStars}> 
+            <option value="0">Calificación mínima</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
           </select>
         </div>
       </div>
