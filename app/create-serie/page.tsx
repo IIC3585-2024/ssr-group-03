656c4a65952730cb72049162f3a65670802589c4
Benyo27 <bgonzalezb@uc.cl>
2024-06-24 00:07:54 -0400

    feat: create serie

---
+++ app/create-serie/page.tsx
@@ -0,0 +1,132 @@
+import Link from "next/link";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import SubmitButton from "@/components/SubmitButton";
+
+export default function CreateSerie({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
+  const createSerie = async (formData: FormData) => {
+    "use server";
+
+    const supabase = createClient();
+
+    const { error } = await supabase
+      .from('series')
+      .insert([
+        {
+          name: formData.get("name") as string,
+          streaming_service: formData.get("streaming_service") as string,
+          n_seasons: formData.get("n_seasons") as string,
+          n_chapter_by_season: formData.get("n_chapter_by_season") as string,
+          description: formData.get("description") as string,
+          category: formData.get("category") as string,
+          image: formData.get("image") as string,
+        },
+      ])
+
+    if (error) {
+      return redirect("/create-serie?message=No se pudo crear la serie. Intenta nuevamente.");
+    }
+
+    return redirect("/");
+  };
+
+  return (
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 my-40">
+      <Link
+        href="/"
+        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+        >
+          <polyline points="15 18 9 12 15 6" />
+        </svg>{" "}
+        Volver
+      </Link>
+
+      <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+        <label className="text-md" htmlFor="name">
+          Nombre
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          name="name"
+          required
+        />
+        <label className="text-md" htmlFor="streaming_service">
+          Servicio de Streaming
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          name="streaming_service"
+          required
+        />
+        <label className="text-md" htmlFor="n_seasons">
+          Cantidad de Temporadas
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          name="n_seasons"
+          required
+        />
+        <label className="text-md" htmlFor="n_chapter_by_season">
+          Cantidad de Capítulos por Temporada
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          name="n_chapter_by_season"
+          required
+        />
+        <label className="text-md" htmlFor="description">
+          Descripción
+        </label>
+        <textarea
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          name="description"
+          required
+        />
+        <label className="text-md" htmlFor="category">
+          Categoría
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          name="category"
+          required
+        />
+        <label className="text-md" htmlFor="image">
+          URL de la Imagen
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-16"
+          name="image"
+          required
+        />
+        <SubmitButton
+          formAction={createSerie}
+          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+          pendingText="Creando..."
+        >
+          Crear
+        </SubmitButton>
+        {searchParams?.message && (
+          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+            {searchParams.message}
+          </p>
+        )}
+      </form>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import AuthButton from "../components/Navbar/AuthButton";
+import Link from "next/link";
 import { createClient } from "@/utils/supabase/server";
 import Navbar from "@/components/Navbar/Navbar";
 import SignUpUserLink from "@/components/LandingPage/SignUpUserLink";
@@ -31,11 +31,16 @@ export default async function Index() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <Navbar />
-
-      <div className="flex-1 flex flex-col gap-4 max-w-4xl px-3">
-          <h2 className="font-bold text-4xl mb-4">Recomendaciones de Series</h2>
+      <div className="flex-1 flex flex-col gap-10 max-w-4xl px-3">
+          <h2 className="font-bold text-4xl">Recomendaciones de Series</h2>
           <SeriesList />
       </div>
+      <Link
+        href="/create-serie"
+        className="py-2 px-4 flex rounded-md no-underline bg-green-700 hover:bg-btn-background-hover"
+      >
+        Añadir Serie
+      </Link>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs"></footer>
     </div>
